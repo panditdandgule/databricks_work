64e9c7c8d73b52b7f80229100cac6d0527246966
panditdandgule777@gmail.com <panditdandgule777@gmail.com>
2023-09-21 13:05:01 +0000

    add all work

---
+++ Example/Getting Started with the Databricks Platform Lab.py
@@ -0,0 +1,28 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Getting Started with the Databricks Platform
+# MAGIC This notebook provides a hands-on review of some of the basic functionality of the Databricks Data Science and Engineering Workspace.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Using %run
+# MAGIC Complex projects of any type can benefit from the ability to break them down into simpler,reusable components.
+# MAGIC In the context of Databricks notebooks, this facility is provided through the %run magic command.
+# MAGIC When used this way, variables,functions and code blocks become part of the current programming context.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Create a Markdown cell
+
+# COMMAND ----------
+
+# MAGIC %md # This is a header
+# MAGIC - bullet 1
+# MAGIC - bullet 2
+# MAGIC - [example link](https://www.databricks.com/)
+
+# COMMAND ----------
+
+
